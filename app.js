e43d48456ee441b1867411cf09f5d939a4752e0c
powerfulsaso <34785107+powerfulsaso@users.noreply.github.com>
2023-09-21 22:34:06 -0500

    Update app.js
    2. GET http://localhost:8000/api/employees?page=1
    
    Devolverá los primeros 2 empleados. Del elemento 0 al elemento 1 del listado

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-/*
-1. GET http://localhost:8000/api/employees
-Devolverá un array JSON con el contenido del fichero "employees.json" que se adjunta al enunciado.
-Es posible añadirlo al código fuente con require('/path/to/employees.json') y guardarlo en una constante.
+/** 
+ * 1. GET http://localhost:8000/api/employees
+ * Devolverá un array JSON con el contenido del fichero "employees.json" que se adjunta al enunciado.
+ * Es posible añadirlo al código fuente con require('/path/to/employees.json') y guardarlo en una constante.
 */
+
 
 //load data
 const employees = require('./data/employees.json');
@@ -13,9 +14,25 @@ const app = express();
 
 //define routes
 app.get('/api/employees', (request, response) => {
-    console.log(request);
-    response.send(employees);
+    queryParams = request.query;
+    page = queryParams.page;
+    if (page) {
+        console.log(`page= ${page}`);
+        if (page == 1) {
+            /**
+             * 2. GET http://localhost:8000/api/employees?page=1
+             * Devolverá los primeros 2 empleados. Del elemento 0 al elemento 1 del listado
+             */
+
+            response.send(employees.slice(0, 2));
+        }
+
+    } else {
+        response.send(employees);
+    }
+
 });
+
 
 //start server
 app.listen(8000, () => {
